02e014bd1d64cb613a50422885e2d4e84d9e56a6
DIVA <ayashrief29@gmail.com>
2020-02-11 21:50:56 +0200

    GridLayout

---
+++ app/src/main/java/com/example/gride/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.gride;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+import android.widget.GridLayout;
+
+public class MainActivity extends AppCompatActivity {
+GridLayout gridLayout;
+    private GridLayout singleEvent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gridLayout=findViewById(R.id.grad);
+    }
+
+
+
+}
